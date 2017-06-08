530412e89ae4d964964a4b6662f8891733783f0f
unknown <crschmit@gmail.com>
2017-06-08 09:49:23 -0500

    added src/index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Christian Schmitt <crschmit>
+ * @Date:   2017-06-08T09:42:31-05:00
+ * @Email:  crschmit@gmail.com
+ * @Filename: index.js
+ * @Last modified by:   crschmit
+ * @Last modified time: 2017-06-08T09:48:06-05:00
+ */
